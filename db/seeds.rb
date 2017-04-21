9dad147b0e7e6054cec1c9d7832a14229192b092
goraccio <t2nkist@gmail.com>
2017-04-21 18:50:22 +0700

    (m) seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+Comment.delete_all
+
 User.create! email: 'admin@example.com', password: '12345678', name: 'Admin', surname: 'Ivanovich'
+
+10.times do
+  User.create! email: Faker::Internet.email,
+               password: '12345678',
+               name: Faker::Name.first_name ,
+               surname: Faker::Name.last_name
+end
+
+30.times do
+  Comment.create! user: User.all.sample,
+                  content: Faker::Lorem.paragraph
+end
